c0f32fcbe46cab96f4121c433905cb1557371c98
Emilse Arias <emilsearias@gmail.com>
2025-05-12 21:04:55 -0300

    agreguamos un ejemplo de evento submit

--- js/app.js
+++ js/app.js
@@ -1,54 +1,61 @@
-const cambiarTitulo = ()=>{
-    console.log('Desde la funcion cambiarTitulo')
-    //tomar el titulo de la card 
-    const titulo = document.getElementById('titulo')
-    console.log(titulo)
-    //modificar el texto del titulo
-    titulo.textContent = 'Nuevo titulo desde JS'
-    titulo.classList.add('text-primary')
-}
+const cambiarTitulo = () => {
+  console.log("Desde la funcion cambiarTitulo");
+  //tomar el titulo de la card
+  const titulo = document.getElementById("titulo");
+  console.log(titulo);
+  //modificar el texto del titulo
+  titulo.textContent = "Nuevo titulo desde JS";
+  titulo.classList.add("text-primary");
+};
 
-const cambiarTituloInnerHTML = ()=>{
-    console.log('desde la fucnion cambiar titulo con innerHTML')
-    //buscar el titulo h5
-    const titulo = document.getElementById('titulo')
-    //modificarlo
-    titulo.innerHTML = 'Soy un nuevo titulo realizado con <b>InnerHTML</b> 😎'
-}
+const cambiarTituloInnerHTML = () => {
+  console.log("desde la fucnion cambiar titulo con innerHTML");
+  //buscar el titulo h5
+  const titulo = document.getElementById("titulo");
+  //modificarlo
+  titulo.innerHTML = "Soy un nuevo titulo realizado con <b>InnerHTML</b> 😎";
+};
 
-const ocultarParrafo = ()=>{
+const ocultarParrafo = () => {
+  //buscar el parrafo
+  const parrafos = document.getElementsByTagName("p");
+  const btnOcultar = document.querySelector(".btn-danger");
+  if (btnOcultar.textContent === "Ocultar párrafo") {
+    //cambiar la clase d-none
+    parrafos[2].classList.add("d-none");
+    btnOcultar.textContent = "Mostrar párrafo";
+  } else {
+    parrafos[2].classList.remove("d-none");
+    btnOcultar.textContent = "Ocultar párrafo";
+  }
+};
+
+const eliminarParrafo = () => {
+  console.log(" aqui deberia eliminar el parrafo");
+  if (borrarParrafo) {
     //buscar el parrafo
-    const parrafos = document.getElementsByTagName('p')
-    const btnOcultar = document.querySelector('.btn-danger')
-    if(btnOcultar.textContent === 'Ocultar párrafo'){
-        //cambiar la clase d-none
-        parrafos[2].classList.add('d-none')
-        btnOcultar.textContent = 'Mostrar párrafo'
-    }else{
-        parrafos[2].classList.remove('d-none')
-        btnOcultar.textContent = 'Ocultar párrafo'
-    }
-}
+    const parrafos = document.getElementsByTagName("p");
+    console.log(parrafos[3]);
+    //luego indicar que se borre
+    parrafos[3].remove();
+    borrarParrafo = false;
+    btnEliminarParrafo.classList.add("disabled");
+  }
+};
 
-const eliminarParrafo = ()=>{
-    console.log(' aqui deberia eliminar el parrafo')
-    if(borrarParrafo){
-        //buscar el parrafo
-        const parrafos = document.getElementsByTagName('p')
-        console.log(parrafos[3])
-        //luego indicar que se borre
-        parrafos[3].remove();
-        borrarParrafo=false
-        btnEliminarParrafo.classList.add('disabled')
-    }
-}
-
+const mostrarNombre = (e) => {
+  e.preventDefault();
+  console.log("aqui deberia mostrar un nombre");
+  console.log(e);
+};
 
 // const btnCambiarTitulo = document.getElementsByClassName('btn-outline-warning')
-const btnCambiarTitulo = document.querySelector('.btn-outline-warning')
-const btnEliminarParrafo = document.getElementById('btnEliminarParrafo')
+const btnCambiarTitulo = document.querySelector(".btn-outline-warning");
+const btnEliminarParrafo = document.getElementById("btnEliminarParrafo");
+// const formulario = document.getElementById("miFormulario");
 let borrarParrafo = true;
 
 //agregar un manejador de eventos
-btnCambiarTitulo.addEventListener('click', cambiarTituloInnerHTML)
-btnEliminarParrafo.addEventListener('click',eliminarParrafo)
+btnCambiarTitulo.addEventListener("click", cambiarTituloInnerHTML);
+btnEliminarParrafo.addEventListener("click", eliminarParrafo);
+// formulario.addEventListener("submit", mostrarNombre);
